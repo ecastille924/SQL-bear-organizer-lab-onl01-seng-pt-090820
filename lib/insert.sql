7e80c7285c3c48244c61655fd161c28576cc6748
Erick Castille <ecastille924@gmail.com>
2020-11-05 00:38:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Cholocalte", 34, "Male", "Brownish", "Saucy", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brownish-Black", "Friendly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "Female", 'Brownish', 'Stately', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "Female", "Brownish", "Stately", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Segeant Brown", 13, "Female", "Brown", "Sisterly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "Female", "Blackish-grey", "Wise", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 64, "Male", "Grey", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "Female", "Black", "Calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 14, "Female", 'Brownish', 'Stately', true);
