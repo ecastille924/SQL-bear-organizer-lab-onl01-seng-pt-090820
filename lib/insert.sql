c087b30bfeb9ddd70d62aa701469ebafb31956cf
Erick Castille <ecastille924@gmail.com>
2020-11-05 00:43:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Cholocalte", 34, "Male", "Brownish", "Saucy", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brownish-Black", "Friendly", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "Female", "Brownish", "Stately", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Segeant Brown", 13, "Female", "Brown", "Sisterly", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "Female", "Blackish-grey", "Wise", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 64, "Male", "Grey", "Rude", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "Female", "Black", "Calm", TRUE);
-INSERT INTO bears (name, age, TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Cholocalte", 34, "Male", "Brownish", "Saucy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brownish-Black", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "Female", "Brownish", "Stately", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Segeant Brown", 13, "Female", "Brown", "Sisterly", 1;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "Female", "Blackish-grey", "Wise", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 64, "Male", "Grey", "Rude", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "Female", "Black", "Calm", 1);
+INSERT INTO bears (name, age, 1);
