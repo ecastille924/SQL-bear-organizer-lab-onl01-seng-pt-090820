8059e2bf89ff7cabd20666a74103a0ddf3b41310
Erick Castille <ecastille924@gmail.com>
2020-11-05 00:59:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Cholocalte', 34, 'M', 'Brownish', 'Saucy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 34, 'M', 'Brownish', 'Saucy', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Brownish-Black', 'Friendly', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'Brownish', 'Stately', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 13, 'F', 'Brown', 'Sisterly', 1;
