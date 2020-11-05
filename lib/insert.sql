80b21426a754b7e1269b03829af1dc5bf661cb58
Erick Castille <ecastille924@gmail.com>
2020-11-05 00:50:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Cholocalte", 34, "Male", "Brownish", "Saucy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brownish-Black", "Friendly", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "Female", "Brownish", "Stately", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Segeant Brown", 13, "Female", "Brown", "Sisterly", 1;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "Female", "Blackish-grey", "Wise", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 64, "Male", "Grey", "Rude", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "Female", "Black", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 37, "Male", "Grey", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Cholocalte", 34, "M", "Brownish", "Saucy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Brownish-Black", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "F", "Brownish", "Stately", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Segeant Brown", 13, "F", "Brown", "Sisterly", 1;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "F", "Blackish-grey", "Wise", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 64, "M", "Grey", "Rude", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "F", "Black", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 37, "M", "Grey", "Aggressive", 0);
